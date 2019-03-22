cc7c9261c1f5dcb0edf3ef9c934677f28059aaa7
niket01 <niket01tv@gmail.com>
2019-03-22 22:41:17 +0400

    task 1 done

--- src/test/java/beru/task_1.java
+++ src/test/java/beru/task_1.java
@@ -1,5 +1,18 @@
 package beru;
 
-public class task_1 {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
 
+public class task_1 {
+    @Test
+    public void execute_login(){
+        chrome.set_driver();
+        chrome.get_beru();
+        //убрать рекламный банер
+        chrome.driver.findElement(By.cssSelector("div._3BBUsZVSt0._3pvYcLe0Ew")).click();
+        //вход на сайт
+        Account.login();
+        chrome.driver.get("https://beru.ru/logout?retpath=https%3A%2F%2Fberu.ru%2F%3Fncrnd%3D3082%26loggedin%3D1");
+        chrome.driver.close();
+    }
 }
